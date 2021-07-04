e8e106f83ee607f2ea500f0401fa11cd61634596
Carlos Eduardo Alves do Nascimento <carloseduardo108090@hotmail.com>
2021-07-04 11:54:50 -0300

    a

--- src/drawer/item.tsx
+++ src/drawer/item.tsx
@@ -19,41 +19,42 @@ export default function CartCard({
 
   return (
     <CartCardContainer
-      className="card-item"
       onMouseLeave={() => setShowClose(false)}
       onMouseEnter={() => setShowClose(true)}
     >
-      <div className="title">
-        <h4>{order.data.titulo}</h4>
-        <Zoom key={showClose.toString()} in={showClose} unmountOnExit>
-          <IconButton
-            onClick={() => onClose(order.id)}
-            size="small"
-            className="icon"
-          >
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        </Zoom>
-      </div>
+      <div className="card-item">
+        <div className="title">
+          <h4>{order.data.titulo}</h4>
+          <Zoom key={showClose.toString()} in={showClose} unmountOnExit>
+            <IconButton
+              onClick={() => onClose(order.id)}
+              size="small"
+              className="icon"
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </Zoom>
+        </div>
 
-      {showDescField ? (
-        <TextField
-          onBlur={() => {
-            setShowDescField(false);
-            onDescEdit(order.id, description);
-          }}
-          style={{ fontSize: 14 }}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          multiline={true}
-          rows={6}
-          inputProps={{ className: "p" }}
-          fullWidth
-          autoFocus
-        />
-      ) : (
-        <p onClick={() => setShowDescField(true)}>{description}</p>
-      )}
+        {showDescField ? (
+          <TextField
+            onBlur={() => {
+              setShowDescField(false);
+              onDescEdit(order.id, description);
+            }}
+            style={{ fontSize: 14 }}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            multiline={true}
+            rows={6}
+            inputProps={{ className: "p" }}
+            fullWidth
+            autoFocus
+          />
+        ) : (
+          <p onClick={() => setShowDescField(true)}>{description}</p>
+        )}
+      </div>
     </CartCardContainer>
   );
 }
